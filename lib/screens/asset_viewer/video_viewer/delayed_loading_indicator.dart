3c8328377183eaae19e4ddc7db14a6b282e055d6
= <=>
2024-07-31 21:36:31 +0100

    feat: video player

---
+++ lib/screens/asset_viewer/video_viewer/delayed_loading_indicator.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class DelayedLoadingIndicator extends StatelessWidget {
+  /// The delay to avoid showing the loading indicator
+  final Duration delay;
+
+  /// Defaults to using the [ImmichLoadingIndicator]
+  final Widget? child;
+
+  /// An optional fade in duration to animate the loading
+  final Duration? fadeInDuration;
+
+  const DelayedLoadingIndicator({
+    super.key,
+    this.delay = const Duration(seconds: 3),
+    this.child,
+    this.fadeInDuration,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Future.delayed(delay),
+      builder: (context, snapshot) {
+        late Widget c;
+        if (snapshot.connectionState == ConnectionState.done) {
+          c = child ??
+              const SizedBox(
+                width: 40,
+                height: 40,
+                key: ValueKey('loading'),
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                ),
+              );
+        } else {
+          c = Container(key: const ValueKey('hiding'));
+        }
+
+        return AnimatedSwitcher(
+          duration: fadeInDuration ?? Duration.zero,
+          child: c,
+        );
+      },
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,18 @@ import Foundation
 
 import bitsdojo_window_macos
 import isar_flutter_libs
+import package_info_plus
 import path_provider_foundation
 import sqflite
+import video_player_avfoundation
+import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
