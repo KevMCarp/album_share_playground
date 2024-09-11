10f097370981bab745b6844391dfcfcd6ff14628
Kevin McDermott-Carpenter <kevin.mcdermott-carpenter@concentechltd.co.uk>
2024-09-11 22:05:05 +0100

    feat: move asset syncing to separate service

--- lib/services/library/library_providers.dart
+++ lib/services/library/library_providers.dart
@@ -3,35 +3,32 @@ import 'dart:typed_data';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../immich/asset_grid/asset_grid_data_structure.dart';
+import '../../models/album.dart';
 import '../../models/asset.dart';
 import '../../models/asset_group.dart';
-import '../../models/library_state.dart';
-import '../api/api_provider.dart';
 import '../database/database_providers.dart';
 import '../files/file_service.dart';
 import '../preferences/preferences_providers.dart';
-import 'library_service.dart';
 
 abstract class LibraryProviders {
-  /// Listens to a list of all assets.
-  static final state =
-      StateNotifierProvider.autoDispose<LibraryService, LibraryState>((ref) {
+  /// Listen to a stream of assets across all albums.
+  static final assets = StreamProvider.autoDispose<List<Asset>>((ref) {
     final db = ref.watch(DatabaseProviders.service);
-    final api = ref.watch(ApiProviders.service);
-    final syncFrequency =
-        ref.watch(PreferencesProviders.service.select((p) => p.syncFrequency));
-
-    return LibraryService(syncFrequency, api, db);
+    return db.assetStream();
   });
 
-  static final renderList = FutureProvider.autoDispose(
-    (ref) {
-      final libraryState = ref.watch(state);
+  /// Listen to a stream of assets for the selected album.
+  static final assetsFor =
+      StreamProvider.autoDispose.family<List<Asset>, Album>((ref, album) {
+    final db = ref.watch(DatabaseProviders.service);
+    return db.assetStream(album);
+  });
+
+  static final renderList =
+      FutureProvider.autoDispose.family<RenderList, List<Asset>>(
+    (ref, assets) {
       final groupBy = ref.watch(PreferencesProviders.groupBy);
-      return libraryState.when(
-        building: () => Future.value(RenderList([], null, [])),
-        built: (assets) => RenderList.fromAssets(assets, groupBy),
-      );
+      return RenderList.fromAssets(assets, groupBy);
     },
   );
 
