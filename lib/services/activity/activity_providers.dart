26518c6f80c26132379d21b499bc12508365f795
Kevin McDermott-Carpenter <kevin.mcdermott-carpenter@concentechltd.co.uk>
2024-10-03 22:59:05 +0100

    fix: own activity shown in notifications sidebar

--- lib/services/activity/activity_providers.dart
+++ lib/services/activity/activity_providers.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logging/logging.dart';
 
 import '../../models/activity.dart';
 import '../../models/album.dart';
 import '../../models/asset.dart';
 import '../api/api_provider.dart';
+import '../auth/auth_providers.dart';
 import '../database/database_providers.dart';
 import 'activity_upload_service.dart';
 
@@ -15,13 +15,12 @@ abstract class ActivityProviders {
   static final notifications =
       StreamProvider.autoDispose<List<Activity>>((ref) {
     final db = ref.watch(DatabaseProviders.service);
-    // final user = ref.watch(AuthProviders.userStream).value;
+    final user = ref.watch(AuthProviders.userStream).value;
 
-    db.activityStream().listen((d) {});
-    return db.activityStream();
-    // }
-
-    // return db.activityStreamForUser(user);
+    if (user == null) {
+      return db.activityStream();
+    }
+    return db.activityStreamForUser(user);
   });
 
   static final assetInAlbum = StreamProvider.autoDispose
